b7d5a35b50b44e6519f4d9a8d64bcaee8b559890
Ernest Juchheim <64336436+juchheim@users.noreply.github.com>
2024-05-21 15:03:16 -0500

    Style revisions to search results

--- wp-content/themes/masd-testing/search.php
+++ wp-content/themes/masd-testing/search.php
@@ -18,7 +18,7 @@ get_header();
 				<h1 class="page-title">
 					<?php
 					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'masd-testing' ), '<span>' . get_search_query() . '</span>' );
+					printf( esc_html__( 'Showing search Results for: %s', 'masd-testing' ), '<span>' . get_search_query() . '</span>' );
 					?>
 				</h1>
 			</header><!-- .page-header -->
